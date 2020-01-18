11db30321f6061f0bc845f1a0dcd669ba899bc5a
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-17 22:40:04 -0500

    BUILD: (Data): Fourth Commit
    - created a dummy database to use as placeholder before using axios / fetch

--- react-guc/src/App.js
+++ react-guc/src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+// IMPORT: utility components
+import axios from 'axios';
+
+// IMPORT: Sample data
+import MyData from "./Data/MyData";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>It's Me</h1>
+        </header>
+        <main>
+        </main>
+      </div>
+    );
+  }
+
 }
 
 export default App;
