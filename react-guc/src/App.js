d695975e21f08290e9138400ae592a2e2c8d7a89
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-17 22:27:51 -0500

    BUILD: (Packages): Third Commit
    - added axios, styled components, stylus, postcss & gulp

---
+++ react-guc/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
